fe695ca8f857e53cd34259d04f228d9769ce3662
phucph <phucph@trobz.com>
2025-10-14 17:33:06 +0700

    [MIG] product_analytic_pos: Migration to 18.0

---
+++ product_analytic_pos/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Product Analytic POS",
+    'summary': """Use analytic account defined on
+                  product for POS orders""",
+    'author': 'Trobz',
+    'website': "https://github.com/OCA/account-analytic",
+    'category': 'Point Of Sale, Accounting',
+    'version': '18.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'point_of_sale',
+        'product_analytic',
+    ],
+    'data': [
+        'data/ir_cron.xml',
+    ],
+}
